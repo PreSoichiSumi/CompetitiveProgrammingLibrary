11a18b9e5467ce1dc4451170f091a24928d60680
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-08-23 22:03:49 +0900

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,25 +3,22 @@
 #include <array>
 #include <cmath>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 #define MODULE 1000000007
 #define MP make_pair
 
+template<class T,class U>inline void chmin(T &t,U f){if(t>f)t=f;}
+template<class T,class U>inline void chmax(T &t,U f){if(t<f)t=f;}
+
 typedef pair<int, int> P;
 typedef long long LL;
-const int INF = INT_MAX / 2;
-const int MAXN = 1000;
+const int INF = INT_MAX / 2;    //int_max->2*e+9
+const int MAXN = 100001;
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
 
 //-----Template---------
-
-int main()
-{
-	cout << "Hello World!\n";
-	cout << "A" << endl;
-	return 0;
-}
