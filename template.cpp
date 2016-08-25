63cdc10e8032d68236bea4f4015156f7a601e5e7
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-08-26 06:56:32 +0900

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -18,7 +18,19 @@ typedef long long LL;
 const int INF = INT_MAX / 2;    //int_max->2*e+9
 const int MAXN = 100001;
 
+/*struct edge {   //structとclassの違いは，メンバが初期状態でpublicとなっているかprivateとなっているか
+    edge(int to, int cost) : to(to), cost(cost) {}
+    int to, cost;
+};
+vector<edge> graph[MAXN];*/
+
+
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
 
 //-----Template---------
+
+int main(){
+	ios::sync_with_stdio(false);
+
+}
