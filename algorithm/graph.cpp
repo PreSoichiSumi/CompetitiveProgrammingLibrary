e796206bc9301b49ee63d444de33390dcbc2f3a1
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-09-08 03:54:50 +0900

    solved arihon 2-4 2-5

--- algorithm/graph.cpp
+++ algorithm/graph.cpp
@@ -1,4 +1,6 @@
-﻿struct edge {   //structとclassの違いは，メンバが初期状態でpublicとなっているかprivateとなっているか
+﻿//-Dijkstra--
+
+struct edge {   //structとclassの違いは，メンバが初期状態でpublicとなっているかprivateとなっているか
     edge(LL to, LL cost) : to(to), cost(cost) {}
 
     LL to, cost;
@@ -36,3 +38,105 @@ public:
         return vector<LL>(dist,dist+N);
     }
 };
+
+//---Bellman-Ford--O(VE)--------------
+#define MAXE 10001
+#define MAXV 10001
+struct edge{int from,to,cost};
+edge es[MAXE];
+int d[MAXV];
+int V,E;
+void shortest_path(int s){
+    for(int i=0;i<V;i++)d[i]=INF;
+    d[s]=0;
+    while (true){
+        bool update=false;
+        for (int i = 0; i < E; ++i) {
+            edge e=es[i];
+            if(d[e.from]!=INF && d[e.to]>d[e.from] +e.cost){
+                d[e.to]=d[e.from] +e.cost;
+                update = true;
+            }
+        }
+        if(!update)break;
+    }
+}
+
+bool findNegativeLoop(){
+    memset(d,0, sizeof(d));
+    for (int i = 0; i < V; ++i) {
+        for (int j = 0; j < E; ++j) {
+            edge e=es[j];
+            if(d[e.to]>d[e.from]+e.cost)
+                d[e.to]=d[e.from]+e.cost;
+            if(i==V-1)return true;
+        }
+    }
+    return false;
+}
+//------------
+
+///---warshal floid---------
+#define MAXV 10001
+int d[MAXV][MAXV];  //存在しないならINF,d[i][i]=0
+int V;
+
+void warshallFloid(){
+    for (int k = 0; k < V; ++k)
+        for (int i = 0; i < V; ++i)
+            for (int j = 0; j < V; ++j)
+                d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
+}
+//--------------------
+
+
+//----prim-------------------
+#define MAXV 1001
+int cost[MAXV][MAXV];
+int mincost[MAXV];
+bool used[MAXV];
+int V;
+int prim(){
+    for (int i = 0; i < V; ++i) {
+        mincost[i]=INF;
+        used[i]=false;
+    }
+    mincost[0]=0;
+    int res=0;
+    while(true){
+        int v=-1;
+        for (int u = 0; u < V; ++u) {
+            if(!used[u] && (v==-1 || mincost[u]<mincost[v]))v=u;
+        }
+        if(v==-1)break;
+        used[v]=true;
+        res+=mincost[v];
+        for (int u = 0; u < V; ++u) {
+            mincost[u]=min(mincost[u],cost[v][u]);
+        }
+        return res;
+    }
+}
+//------
+
+//-----kruskal---
+bool comp(const edge& e1,const edge& e2){
+    return e1.cost<e2.cost;
+}
+#define MAXE 50000
+edge es[MAXE+1];
+int V,E;
+int kruskal(){
+    sort(es,es+E,comp);
+    UnionFind uf(V);
+    int res=0;
+    for (int i = 0; i < E; ++i) {
+        edge e= es[i];
+        if(!uf.isSame(e.from, e.to)){
+            uf.unite(e.from,e.to);
+            res+=e.cost;
+        }
+    }
+    return res;
+}
+//---------------------
--- algorithm/math.cpp
+++ algorithm/math.cpp
@@ -65,3 +65,35 @@ LL modComb(LL n, LL k, LL mod) {
     return result;
 }
 //------------------
+
+
+//------UnionFind---------
+struct UnionFind {
+    vector<int> data;
+
+    UnionFind(int size) : data(size, -1) {}
+
+    bool unite(int x, int y) {   //併合
+        x = root(x);
+        y = root(y);
+        if (x != y) {
+            if (data[y] < data[x]) swap(x, y);
+            data[x] += data[y];
+            data[y] = x;
+        }
+        return x != y;
+    }
+
+    bool isSame(int x, int y) {    //同じ集合か判定
+        return root(x) == root(y);
+    }
+
+    int root(int x) {
+        return data[x] < 0 ? x : data[x] = root(data[x]);
+    }
+
+    int size(int x) {
+        return -data[root(x)];
+    }
+};
+//--------------
