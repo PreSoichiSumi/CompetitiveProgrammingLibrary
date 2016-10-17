8a161b60417bdd1ba5860e846d5385b749927e3a
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-10-17 15:23:33 +0900

    solve arc062e-cube

--- solved/AtCoder/ARC/062E-Cube-先に決めて組み合わせ計算．先に決めるのを全探索.cpp
+++ solved/AtCoder/ARC/062E-Cube-先に決めて組み合わせ計算．先に決めるのを全探索.cpp
@@ -4,9 +4,9 @@
 //priority_queue< pair<int,pair<int,int> >, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int> > > > pqueue;    //cost, vertex(行き先)
 using namespace std;
 
-#define MODULE 1000000007
-#define MP make_pair
-#define ALL(v) (v).begin(),(v).end()
+#define module 1000000007
+#define mp make_pair
+#define all(v) (v).begin(),(v).end()
 
 template<class T, class U>
 inline void chmin(T &t, U f) { if (t > f)t = f; }
@@ -29,87 +29,122 @@ int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
 
 //-----Template---------
-void swap2(string &s, int i, int j) {
-    char c = s[i];
-    s[i] = s[j];
-    s[j] = c;
-}
-
-string getMinStr(string &s) {    //4*4->16
-    string m = s;
-    for (int i = 0; i < 3; ++i) {
-        chmin(m, s.substr(1 + i, 4 - (1 + 1)) + s.substr(0, i + 1));    //substr(i,j)iから長さjぶん
+//pure
+LL compress(vector<int>& v){ //10^3*4->10^12
+    LL r=0;
+    for (int i = 0; i < v.size(); ++i) {
+        r=r*1000+v[i];
     }
-    return m;
+    return r;
 }
 
+vector<int> getMinVector(vector<int> v){
+    vector<vector<int>> tmpVecs;
+    tmpVecs.push_back(v);
+    for (int i = 1; i < 4; ++i) {
+        rotate(v.begin(),v.begin()+1,v.end());  //begin to end, begin+1が先頭に来るように左に回す
+                                                //右に回すときには rotate(v.rbegin(),v.rbegin()+1,v.rend()) とrbeginを使う
+        tmpVecs.push_back(v);
+    }
+    //sort(all(tmpVecs));   //こっちでも良いけど，lambda練習用に↓
+    sort(tmpVecs.begin(),tmpVecs.end(),[](vector<int>& a,vector<int>& b){//ちゃんと参照渡しするように
+        if(a.size()==0)return true;
+        for (int i = 0; i < a.size(); ++i) {
+            if(a[i]!=b[i]){
+                return a[i]<b[i];
+            }
+        }
+        return true;
+    });
+    return tmpVecs[0];
+}
+//pure
+int getWay(vector<int> v){
+    set<vector<int>> s;
+    for (int i = 0; i < 4; ++i) {
+        s.insert(v);
+        rotate(v.begin(),v.begin()+1,v.end());
+    }
+    return 4/s.size();
+}
+//pure　こっちでもいい
+/*int getWay(vector<int>& v){
+    int wayNum=1;
+    if(v[0] == v[1] && v[1]==v[2] && v[2]==v[3]){
+        wayNum=4;
+    }else if(v[0]==v[2] && v[1]==v[3]){
+        wayNum=2;
+    }
+    return wayNum;
+}*/
 
-int n, counter;
-string tile[401];
-string tmp;
-unordered_map<string,int> memo;
-unordered_map<string,int> way;
+int n;
+vector<vector<int>> tile(401);
+unordered_map<LL,LL> cnt;
+unordered_map<LL,LL> way;
 
 int main() {
     ios::sync_with_stdio(false); //cout<< fixed << setprecision(10);
     cin >> n;
     for (int i = 0; i < n; ++i) {
-        cin >> tmp;
-        tile[i] = getMinStr(tmp);
-        memo[tile[i]]++;
-    }
-    for(auto it:memo){
-        int wayNum=1;
-        if(it.first[0] == it.first[1] && it.first[1]==it.first[2] && it.first[2]==it.first[3]){
-            wayNum=4;
-        }else if(it.first[0]==it.first[2] && it.first[1]==it.first[3]){
-            wayNum=2;
+        vector<int> v(4);
+        for (int j = 0; j < 4; ++j) {
+            cin>>v[j];
         }
-        way[it.first]=wayNum;
+        v=getMinVector(v);
+        tile[i]=v;
+        LL minTile=compress(v);
+        cnt[minTile]++;
+        way[minTile]= getWay(v);
     }
-    int ans=0;
+    LL ans=0;
     for (int i = 0; i < n; ++i) {
+        vector<int> ue = tile[i];
+        LL ueLL=compress(ue);
+        cnt[ueLL]--;
         for (int j = i + 1; j < n; ++j) {
-            for (int k = 0; k < 4; ++k) {   //上を回転
+            vector<int> sita = tile[j];
+            LL sitaLL=compress(sita);
+            cnt[sitaLL]--;
 
-                string ue = tile[i];        //0-1:mae   2-3:usiro
-                string sita = tile[j];    //3-2:mae  	0-1:usiro
-                swap2(sita, 0, 2);
-                swap2(sita, 1, 3);
-                //std::swap(sita[1],sita[3]);
-                map<string,int> sokuTile;
-                for (int l = 0; l < 4; ++l) {   //側面
-                    string tmpTile;
-                    if (l != 3) {
-                        tmpTile += ue.substr(l, 2);
-                        tmpTile += sita.substr(l, 2);
-                    } else {
-                        tmpTile += ue[3];
-                        tmpTile += ue[0];
-                        tmpTile += sita[3];
-                        tmpTile += sita[0];
-                    }
-                    tmpTile=getMinStr(tmpTile);
-                    sokuTile[tmpTile]++;
+            swap(sita[1],sita[3]);
+            for (int k = 0; k < 4; ++k) {   //sitaを回転
+                map<LL,int> sokuTile;
+                for (int l = 0; l < 4; ++l) {   //側面のタイルを書き出す
+                    vector<int> tmpTile(4);
+                    tmpTile[0]=ue[l];
+                    tmpTile[1]=ue[(l+3)%4];
+                    tmpTile[2]=sita[(l+3)%4];
+                    tmpTile[3]=sita[l];
+                    /*tmpTile[0]=ue[(l+1)%4];   こっちはらて氏のパクリ
+                    tmpTile[1]=ue[l];
+                    tmpTile[2]=sita[l];
+                    tmpTile[3]=sita[(l+1)%4];*/
+
+                    tmpTile=getMinVector(tmpTile);
+                    sokuTile[compress(tmpTile)]++;
                 }
-                int resTmp=1;
+                LL resTmp=1;
                 for (auto it : sokuTile) {
-                    if(memo.find(it.first)==memo.end()){
-                        resTmp*=0; continue;
+                    if(cnt.find(it.first)==cnt.end()){
+                        resTmp*=0; break;
                     }
 
                     if(it.second==1){
-                        resTmp*=memo[it.first]*way[it.first];
+                        resTmp*=cnt[it.first]*way[it.first];
                     }else{
                         for (int l = 0; l < it.second; ++l) {
-                            resTmp*=(memo[it.first]-l)*way[it.first];
+                            resTmp*=(cnt[it.first]-l)*way[it.first];
                         }
                     }
                 }
                 ans+=resTmp;
 
+                rotate(sita.begin(),sita.begin()+1,sita.end());
             }
+            cnt[sitaLL]++;
         }
+        cnt[ueLL]++;
     }
-
+    cout<<ans/3<<endl;
 }
