af19973dae7f3b181a7bb8aa2b410b1632b1b91a
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-11-20 19:34:57 +0900

    refactor prime library

--- library/math/prime.cpp
+++ library/math/prime.cpp
@@ -1,32 +1,33 @@
-//---ŽŽ‚µŠ„‚è‘f””»’è O(ãn)
-bool isPrime(int n){
-    for (int i = 2; i*i<=n; ++i) {
-        if(n%i==0)return false;
+//試し割りによる素数判定
+bool isPrime(int n) {
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0)return false;
     }
-    return n!=1;
+    return n != 1;
 }
-//---ƒGƒ‰ƒgƒXƒeƒlƒX‚Ì‚Ó‚é‚¢  nˆÈ‰º‚Ì‘f”‚ð—ñ‹“--------------- O(nlog(logn))
-vector<LL> getPrimesEratos(LL n){ 
-    LL rootN= static_cast<long>(sqrt(n));
-    bool prime[n+1];
-    fill(prime,prime+n+1,true);
-    prime[0]=prime[1]=false;
-    for (LL i = 2; i <= rootN ; i++) {  //ãn‚ð’´‚¦‚È‚¢‚·‚×‚Ä‚Ì‘f”‚ÅŠ„‚èØ‚ê‚È‚¯‚ê‚În‚Í‘f”
-        if(prime[i]) {
-            for (LL j = i * 2; j <= n; j+=i) prime[j] = false;
+
+//エラトステネスの篩による素数判定 O( n log( log n ) )
+vector <LL> getPrimesEratos(LL n) {
+    LL rootN = static_cast<long>(sqrt(n));
+    bool prime[n + 1];
+    fill(prime, prime + n + 1, true);
+    prime[0] = prime[1] = false;
+    for (LL i = 2; i <= rootN; i++) {
+        if (prime[i]) {
+            for (LL j = i * 2; j <= n; j += i) prime[j] = false;
         }
 
     }
-    vector<LL> res;
-    for(LL i=0;i<=n;i++){
-        if(prime[i])
+    vector <LL> res;
+    for (LL i = 0; i <= n; i++) {
+        if (prime[i])
             res.push_back(i);
     }
     return res;
 }
 
-//‘fˆö”•ª‰ð
-map<int, int> primeDecomposition(LL num, vector<LL> &primes) {
+//素因数分解
+map<int, int> primeDecomposition(LL num, vector <LL> &primes) {
     LL tmp = num;
     map<int, int> res;
 
@@ -43,6 +44,8 @@ map<int, int> primeDecomposition(LL num, vector<LL> &primes) {
     return res;
 }
 
+//2以上の約数の数え上げ   O( 素因数の数 )
+//引数は素因数分解の結果
 int countDivisor(map<int, int> decomposed) {
     int counter = 1;
     for (auto it = decomposed.begin(); it != decomposed.end(); it++) {
