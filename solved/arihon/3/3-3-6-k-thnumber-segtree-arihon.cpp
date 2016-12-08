0f9a317d02e1935e5565f60db1eafc5a1f2b654d
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-12-08 09:55:18 +0900

    implement 3-3-6 segtree

---
+++ solved/arihon/3/3-3-6-k-thnumber-segtree-arihon.cpp
@@ -0,0 +1,77 @@
+#include <climits>
+#include <utility>
+#include <vector>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define MODULE 1000000007
+#define MP make_pair
+#define ALL(v) (v).begin(),(v).end()
+#define fi first
+#define se second
+typedef pair<int, int> P;
+typedef long long LL;
+
+#define MAX_N 100010
+#define MAX_M 5001
+int nums[MAX_N];
+int N, M;
+int A[MAX_N];
+const int ST_SIZE = (1 << 18) - 1;
+vector<int> dat[ST_SIZE];
+
+void init(int k, int l, int r) {
+    if (r - l == 1) {
+        dat[k].push_back(A[l]);
+    } else {
+        int lch = k * 2 + 1, rch = k * 2 + 2;
+        init(lch, l, (l + r) / 2);
+        init(rch, (l + r) / 2, r);
+        dat[k].resize(r - l);
+        merge(dat[lch].begin(), dat[lch].end(), dat[rch].begin(), dat[rch].end(), dat[k].begin());
+    }
+}
+
+int query(int i, int j, int x, int k, int l, int r) {
+    if (j <= l || r <= i) {
+        return 0;
+    } else if (i <= l && r <= j) {
+        return upper_bound(dat[k].begin(), dat[k].end(), x) - dat[k].begin();
+    } else {
+        int lc = query(i, j, x, k * 2 + 1, l, (l + r) / 2);
+        int rc = query(i, j, x, k * 2 + 2, (l + r) / 2, r);
+        return lc + rc;
+    }
+}
+
+int main() {
+    //ios::sync_with_stdio(false); //cout<< fixed << setprecision(10);
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < N; ++i) {
+        scanf("%d", &A[i]);
+    }
+    for (int i = 0; i < N; ++i) {
+        nums[i] = A[i];
+    }
+    sort(nums, nums + N);
+    init(0, 0, N);
+    for (int i = 0; i < M; i++) {
+        int l, r, k;
+        scanf("%d%d%d", &l, &r, &k);
+        l--;
+        int lb = -1, ub = N - 1;
+        while (ub - lb > 1) {
+            int md = (ub + lb) / 2;
+            int c = query(l, r, nums[md], 0, 0, N);
+            if (c >= k)ub = md;
+            else {
+                lb = md;
+            }
+        }
+        printf("%d\n", nums[ub]);
+    }
+
+}
