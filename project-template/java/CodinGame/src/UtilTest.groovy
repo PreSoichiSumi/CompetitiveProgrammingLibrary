3557d5b8be932f53a30c81938c1fe1ac0890afaf
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-12-10 20:10:08 +0900

    add codingame template

---
+++ project-template/java/CodinGame/src/UtilTest.groovy
@@ -0,0 +1,11 @@
+import org.junit.Test
+
+/**
+ * Created by s-sumi on 2016/12/04.
+ */
+class UtilTest {
+    @Test
+    void testIsIntersect() {
+        //Test.assrtEqual(Util.isIntersect());
+    }
+}
