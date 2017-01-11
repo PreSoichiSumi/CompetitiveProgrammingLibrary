da34799202f7f92b89165e18d19a5aaad8e8d871
SoichiSumi <s-sumi@ist.osaka-u.ac.jp>
2017-01-12 01:57:33 +0900

    solve abc04d partial pts

---
+++ solved/AtCoder/ABC/04D-partial.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+//#include <boost/functional/hash.hpp>
+//example: unordered_set< pair<int,int>,boost::hash< std::pair<int, int> > > used;
+//priority_queue< pair<int,pair<int,int> >, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int> > > > pqueue;    //cost, vertex(行き先)
+using namespace std;
+
+#define MODULE 1000000007
+#define MP make_pair
+#define ALL(v) (v).begin(),(v).end()
+#define fi first
+#define se second
+
+template<class T,class U>inline void chmin(T &t,U f){if(t>f)t=f;}
+template<class T,class U>inline void chmax(T &t,U f){if(t<f)t=f;}
+template<typename A, size_t N, typename T>
+inline void Fill(A (&array)[N], const T &val){ //usage: int dp[10][10]; Fill(dp,INF);
+    std::fill( (T*)array, (T*)(array+N), val );
+}
+
+typedef pair<int, int> P;
+typedef long long LL;
+const int INF = INT_MAX / 2;    //int_max->2*e+9 LLの時はLLONG_MAX
+/*const int MAXN = 100001;
+
+struct edge {
+    edge(int to, int cost) : to(to), cost(cost) {}
+    int to, cost;
+};
+vector<edge> graph[MAXN];
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};*/
+
+//-----Template---------
+int r,g,b;
+int main(){
+    ios::sync_with_stdio(false); //cout<< fixed << setprecision(10);
+    cin>>r>>g>>b;
+
+    assert(r<=40);
+    assert(g<=40);
+    assert(b<=40);
+
+    //あとrgbの範囲がかぶる場合の考察
+    //なるべくr,bから遠いとこにモノを置きたくない
+
+    int res=0;
+    r--;g--;b--;
+    for (int i = r/2; i >=1 ; --i) {
+        res+=i;
+    }
+    for (int i = r-r/2; i >=1 ; --i) {
+        res+=i;
+    }
+    for (int i = g/2; i >=1 ; --i) {
+        res+=i;
+    }
+    for (int i = g-g/2; i >=1 ; --i) {
+        res+=i;
+    }
+    for (int i = b/2; i >=1 ; --i) {
+        res+=i;
+    }
+    for (int i = b-b/2; i >=1 ; --i) {
+        res+=i;
+    }
+
+    cout<<res<<endl;
+
+}
