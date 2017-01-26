c7f554c02315ec3e3ddfecefac5286220810a378
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2017-01-26 22:55:16 +0900

    add library

---
+++ library/math/mod_pow,comb,felmat.cpp
@@ -0,0 +1,14 @@
+﻿//lim以下のフィボナッチ数を取得する
+vector<LL> getFib(LL lim){
+    vector<LL> res;
+    res.push_back(1);
+    res.push_back(1);
+    LL it=2;
+    LL tmp=1;
+    while(tmp<lim){
+        tmp=res[it-1]+res[it-2];
+        res.push_back(tmp);
+        it++;
+    }
+    return res;
+}
